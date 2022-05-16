ba99f0ed6e9b2eb4b8c9691a6c3c3afb63811734
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-15 20:47:47 -0400

    working model of header selection identifier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React, { useState } from 'react';
 import Header from './components/Header';
+import './App.css';
 
 
 function App() {
+
+  const [projectType] = useState([
+    { name: 'JavaScript', description: 'pure js projects' },
+    { name: 'NoSQL', description: 'NoSQL projects' },
+    { name: 'Node/js', description: 'node/js focused' },
+    { name: 'mySQL', description: 'mySQL focused' },
+  ]);
+
+  const [currentProjectType, setCurrentProjectType] = useState(projectType[0]);
+
   return (
     <div>
-      <Header></Header>
+      <Header
+        projectType={projectType}
+        setCurrentProjectType={setCurrentProjectType}
+        currentProjectType={currentProjectType}       
+      ></Header>
+      <main>
+        
+      </main>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,31 +2,75 @@
 -                          Header
 ------------------------------------------------------------*/
 
+import { useEffect } from "react";
+import {capitalizeFirstLetter} from '../../utils/helper';
 
-function Header() {
+
+function Header(props) {
+
+  const {
+    projectType = [],
+    currentProjectType,
+    setCurrentProjectType,
+    projectTypeSelected,
+    setProjectTypeSelected  
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentProjectType.name);
+  }, [currentProjectType]);
+
   return (
+
     <header className="fle-row px-1">
+
       <h2>
         <a href="/">
-          <span role="img" aria-label="logo">👨‍💻</span> @vieiragcv
+          <span role="img" aria-label="camera">👨‍💻</span> @vieiragcv
         </a>
       </h2>
 
-      <nav>
-
+      <nav
+        projectType={projectType}
+        setCurrentProjectType={setCurrentProjectType}
+        projectTypeSelected={projectTypeSelected}
+        setProjectTypeSelected={setProjectTypeSelected}
+      >
         <ul className="flex-row">
 
-          <li className="mx-2">About Me</li>
-          <li className="mx-2">Projects</li>
-          <li className="mx-2">Resume</li>
-          <li className="mx-2">Contact</li>
+          <li className="mx-2">
+            <a href="#about">About Me</a>
+          </li>
 
+          <li className="mx-2">
+            <a href="#resume">Resume</a>
+          </li>
+
+          <li className="mx-2">
+            <a href="#contact">Contact</a>
+          </li>
+         
+
+          {projectType.map((type) => (
+            <li
+              className = {`mx-1 ${
+                currentProjectType.name === type.name && 'navActive'
+              }`}
+              key={type.name}
+            >
+              <span
+                onClick = { () => {
+                  setCurrentProjectType(type);
+                }}
+              >
+                {capitalizeFirstLetter(type.name)}
+              </span>
+            </li>
+          ))}
         </ul>
-
       </nav>
-
     </header>
-  )
+  );
 }
 
 export default Header;
